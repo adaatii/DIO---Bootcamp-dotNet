5fdb8f16d3b0ae8ee5f965d948ff0e66feff15ce
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-26 13:14:26 -0300

    Trabalhando com Arquivos e Streams em C#
    DirectoryInfo - O programa retorna no console o conteudo do diretório.

---
+++ Trabalhando com Arquivos e Streams em C#/LerDiretorios/Program.cs
@@ -0,0 +1,32 @@
+﻿var path = @"caminho do diretório";
+
+LerDiretorios(path);
+Console.WriteLine("Pressione enter para finalizar");
+Console.ReadLine();
+
+static void LerDiretorios(string path)
+{
+
+    if (Directory.Exists(path))
+    {
+        var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+        foreach (var dir in diretorios)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+            Console.WriteLine($"[Nome]: {dirInfo.Name}");
+            Console.WriteLine($"[Raiz]: {dirInfo.Root}");
+            if (dirInfo.Parent != null)
+            {
+                Console.WriteLine($"[Pai]: {dirInfo.Parent.Name}");
+            }
+
+            Console.WriteLine("-----------------");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{path} não existe");
+    }
+
+}
+
