89aa8db873b072c3052591f33130e3b8202c2c78
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-25 22:46:21 -0300

    Trabalhando com Arquivos e Streams em C#
    Em aula foi criado um arquivo txt no diretório corrente com a inclusão de 3 linhas utilizando StreamWriter.

---
+++ Trabalhando com Arquivos e Streams em C#/FILE_AND_FILEINFO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+
+var path = Path.Combine (Environment.CurrentDirectory, "teste.txt");
+
+//interface IDisposable - pode ter finalização forçada pela aplicação 
+//(neste caso faz o Flush de forma automatica) 
+
+using var sw = File.CreateText(path);
+sw.WriteLine("Esta é a linha 1 do arquivo");
+sw.WriteLine("Esta é a linha 2 do arquivo");
+sw.WriteLine("Esta é a linha 3 do arquivo");
+
+//A linhas só são descarregadas da memória para o arquivo após ser dados um Flush,
+//caso contrário o StreamWriter continua em memória pois não foi finalizado de forma correta. 
+
+/*var sw = File.CreateText(path);
+sw.WriteLine("Esta é a linha 1 do arquivo");
+sw.WriteLine("Esta é a linha 2 do arquivo");
+sw.WriteLine("Esta é a linha 3 do arquivo");
+sw.Flush();*/
