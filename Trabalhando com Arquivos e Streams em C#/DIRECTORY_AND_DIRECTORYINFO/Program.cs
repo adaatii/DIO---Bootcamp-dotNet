3e1d62ac997771064da43b8ead26f36b3f06d56a
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-26 12:47:31 -0300

    Trabalhando com Arquivos e Streams em C#
    Criação de arquivos .txt, movimentação do arquivo entre pastas, renomeação do arquivo no processo de cópia.

--- Trabalhando com Arquivos e Streams em C#/DIRECTORY_AND_DIRECTORYINFO/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/DIRECTORY_AND_DIRECTORYINFO/Program.cs
@@ -1,18 +1,80 @@
 ﻿using System.IO;
 
-var path = Path.Combine(Environment.CurrentDirectory,"globo");
-var dirGlobo = Directory.CreateDirectory(path);
-var dirAmNorte  = dirGlobo.CreateSubdirectory("América do Norte");
-var dirAmCentral  = dirGlobo.CreateSubdirectory("América Central");
-var dirAmSul  = dirGlobo.CreateSubdirectory("América do Sul");
+CriarDiretoriosGlobo();
+CriarArquivo();
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "América do Sul", "Argentina", "argentina.txt");
 
-dirAmNorte.CreateSubdirectory("USA");
-dirAmNorte.CreateSubdirectory("Mexico");
-dirAmNorte.CreateSubdirectory("Canada");
+//MoverArquivo(origem, destino);
+CopiarArquivo(origem,destino);
 
-dirAmCentral.CreateSubdirectory("Costa Rica");
-dirAmCentral.CreateSubdirectory("Panamá");
+static void CopiarArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo de Origem não existe");
+        return;
+    }
 
-dirAmSul.CreateSubdirectory("Brasil");
-dirAmSul.CreateSubdirectory("Argentina");
-dirAmSul.CreateSubdirectory("Paraguai");
+    if (File.Exists(pathDestino))
+    {
+        Console.WriteLine("Arquivo já existe na pasta de destino");
+        return;
+    }
+    
+    File.Copy(pathOrigem, pathDestino);
+}
+
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo de Origem não existe");
+        return;
+    }
+
+    if (File.Exists(pathDestino))
+    {
+        Console.WriteLine("Arquivo já existe na pasta de destino");
+        return;
+    }
+
+    File.Move(pathOrigem, pathDestino);
+}
+
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: ...MM");
+        sw.WriteLine("IDH: 0,...");
+        sw.WriteLine("Dados atualizados: ....");
+    }
+}
+
+static void CriarDiretoriosGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    if (!Directory.Exists(path))
+    {
+        var dirGlobo = Directory.CreateDirectory(path);
+        var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+
+        dirAmNorte.CreateSubdirectory("USA");
+        dirAmNorte.CreateSubdirectory("Mexico");
+        dirAmNorte.CreateSubdirectory("Canada");
+
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmCentral.CreateSubdirectory("Panamá");
+
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguai");
+    }
+
+
+}
