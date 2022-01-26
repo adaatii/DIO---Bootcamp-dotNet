263177aeeb699d7f59c3a20320bec31dbe45a0ef
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-26 11:42:54 -0300

    Trabalhando com Arquivos e Streams em C#
    Criação de diretórios e sub diretórios.

---
+++ Trabalhando com Arquivos e Streams em C#/DIRECTORY_AND_DIRECTORYINFO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+var path = Path.Combine(Environment.CurrentDirectory,"globo");
+var dirGlobo = Directory.CreateDirectory(path);
+var dirAmNorte  = dirGlobo.CreateSubdirectory("América do Norte");
+var dirAmCentral  = dirGlobo.CreateSubdirectory("América Central");
+var dirAmSul  = dirGlobo.CreateSubdirectory("América do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canada");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panamá");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
