bfbbe13852cd0f862b17aa9a6c574d5dcf09becd
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-26 11:18:10 -0300

    Trabalhando com Arquivos e Streams em C#
    Realizado em aula o tratamento do erro carcter invalido.Na primeira solução o programa retorna uma mensagem ao usuario caso seja inserido um nome de arquivo invalido.Na segunda solução foi feita a criação de um 'foreach' para realizar a substituição do caracter invalido por '-'.

--- Trabalhando com Arquivos e Streams em C#/FILE_AND_FILEINFO/Program.cs
+++ Trabalhando com Arquivos e Streams em C#/FILE_AND_FILEINFO/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System.IO;
+using static System.Console;
+
+WriteLine("Digite o nome do arquivo");
+
+var nome = ReadLine();
+
+nome = LimparNome(nome);
+
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
+
+CriarArquivo(path);
+
+WriteLine("Aperte Enter para finalizar");
+ReadLine();
 
 
-var path = Path.Combine (Environment.CurrentDirectory, "teste.txt");
+//substitui caracteres invalidos por '-'
+static string LimparNome(string nome)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(@char, '-');
+    }
+    return nome;
+    
+
+}
 
 //interface IDisposable - pode ter finalização forçada pela aplicação 
 //(neste caso faz o Flush de forma automatica) 
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Esta é a linha 1 do arquivo");
+        sw.WriteLine("Esta é a linha 2 do arquivo");
+        sw.WriteLine("Esta é a linha 3 do arquivo");
+        sw.Flush();
+    }
+    catch (System.Exception)
+    {
 
-using var sw = File.CreateText(path);
-sw.WriteLine("Esta é a linha 1 do arquivo");
-sw.WriteLine("Esta é a linha 2 do arquivo");
-sw.WriteLine("Esta é a linha 3 do arquivo");
+        WriteLine("O nome do arquivo é invalido");
+    }
+
+}
+
+
 
 //A linhas só são descarregadas da memória para o arquivo após ser dados um Flush,
 //caso contrário o StreamWriter continua em memória pois não foi finalizado de forma correta. 
