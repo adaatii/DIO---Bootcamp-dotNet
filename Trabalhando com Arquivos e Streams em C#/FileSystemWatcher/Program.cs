e21e4f40f4fd929c1cc066cf040af1ad4cff1612
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-26 14:27:28 -0300

    Trabalhando com Arquivos e Streams em C#
    monitoramento de todo tipo de alteração no diretorio utilizando o FileSystemWatcher.

---
+++ Trabalhando com Arquivos e Streams em C#/FileSystemWatcher/Program.cs
@@ -0,0 +1,28 @@
+﻿var path = @"/home/adaatii/Documentos/Github/DIO---Curso-de-dotNet/Trabalhando com Arquivos e Streams em C#/DIRECTORY_AND_DIRECTORYINFO/globo";
+
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Deleted += OnDeleted;
+fsw.Renamed += OnRenamed;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine($"Monitoranto eventos {path}");
+Console.WriteLine("Pressione enter para finaliza");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi excluído o arquivo {e.Name}");
+}
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"Foi renomeado o arquivo {e.OldName} para {e.Name}");
+}
