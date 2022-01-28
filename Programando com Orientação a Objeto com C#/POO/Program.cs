de517b407c8f994b9eb22c7cd951d8a6a77ccdae
adaatii <80161447+adaatii@users.noreply.github.com>
2022-01-28 16:25:48 -0300

    Programando com Orientação a Objeto com C#
    Exemplo de Encapsulamento

--- Programando com Orientação a Objeto com C#/POO/Program.cs
+++ Programando com Orientação a Objeto com C#/POO/Program.cs
@@ -6,12 +6,26 @@ namespace POO
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
 
-            p1.Nome = "Bob";
-            p1.Idade = 20;
 
-            p1.Apresentar();
+            //Valores válidos
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(30, 30);
+            Console.WriteLine($"Área: {r.ObterArea()}");
+
+            //Valores inválidos
+            Retangulo r2 = new Retangulo();
+            r.DefinirMedidas(0, 30);
+            Console.WriteLine($"Área: {r2.ObterArea()}");
+
+
+
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
         }
     }
 }
